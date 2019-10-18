0ec8757b5b52cb7dc65c31949bf2aa999ffae666
ping <513011631@qq.com>
2019-10-18 21:17:32 +0800

    def

--- 1.py
+++ 1.py
@@ -55,6 +55,29 @@
 # print('end')
 
 
-a = (1, 2, 3)
-d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
-b = (1, [2, 3])
+# a = (1, 2, 3)
+# d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+# b = (1, [2, 3])
+
+# n1 = 255
+# n2 = 1000
+# print(hex(n1))
+# print(max(n1,n2))
+
+import math
+
+def quadratic(a, b, c):
+    dart=b*b-4*a*c
+    sqr= math.sqrt(dart)
+    x1= (-b+sqr)/(2*a)
+    x2= (-b-sqr)/(2*a)
+    return x1,x2
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+
+if quadratic(2, 3, 1) != (-0.5, -1.0):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0):
+    print('测试失败')
+else:
+    print('测试成功')
