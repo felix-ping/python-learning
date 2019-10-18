4a7af40a9fbd07159dee84e054590e9275ef124b
ping <513011631@qq.com>
2019-10-18 20:35:06 +0800

    if语句

--- 1.py
+++ 1.py
@@ -5,10 +5,33 @@
 # a=input()
 # b=input()
 # print(1024*768,a+b)
-n= input()
-if n < 5:
-    print('xiao')
-elif n >= 5 or n<10:
+
+
+# n=int(input())
+# if n < 5:
+#     print('xiao')
+# elif n >= 5 or n<10:
+#     print('zhong')
+# else :
+#     print('da')
+
+# s1 = 72
+# s2 = 85
+# r =s2-s1
+# print('' % r)
+s1 = input()
+s2 =input()
+weight = float(s2)
+height = float(s1)
+print(height*height)
+ibm = weight/(height*height)
+if ibm <18.5:
+    print('qing')
+elif ibm >=18.5 and ibm <25:
+    print('zhengchang')
+elif ibm >=25 and ibm <28:
     print('zhong')
-else :
-    print('da')
+elif ibm >=28 and ibm <32:
+    print('guozhong')
+else:
+    print('fei')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+a=input()
+b=input()
+print(1024*768,a+b)
+n= input()
+if n < 5:
+    print('xiao')
+elif n >= 5 or n<10:
+    print('zhong')
+else :
+    print('da')
