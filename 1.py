9c463c04bfb4549b3bb6b2f21b1d7b3215893b7c
ping <513011631@qq.com>
2019-10-18 15:29:38 +0800

    练习示例

---
+++ 1.py
@@ -0,0 +1,14 @@
+# name = input('please enter your name:')
+# print('hello',name)
+
+
+# a=input()
+# b=input()
+# print(1024*768,a+b)
+n= input()
+if n < 5:
+    print('xiao')
+elif n >= 5 or n<10:
+    print('zhong')
+else :
+    print('da')
